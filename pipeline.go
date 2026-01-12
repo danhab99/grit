2da734f65ac50801a1a3ae57f5ea85bd8a6d1712
Dan Habot <dan.habot@gmail.com>
2026-01-11 23:07:50 -0500

    new pipeline

---
+++ pipeline.go
@@ -0,0 +1,262 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"sync"
+
+	"github.com/danhab99/idk/chans"
+	"github.com/danhab99/idk/workers"
+)
+
+type Pipeline struct {
+	db *Database
+}
+
+func NewPipeline(d *Database) Pipeline {
+	return Pipeline{d}
+}
+
+func (p *Pipeline) Execute(startStepName string, maxParallel int) int {
+	db := p.db
+	numberOfExecutions := 0
+
+	steps := db.ListSteps()
+
+	msgBus := make(map[string]chan Task)
+
+	var wg sync.WaitGroup
+
+	resourcePool := make(chan struct{}, 20) // capacity = max concurrent work
+
+	stepsCount, err := db.CountStepsWithoutParallel()
+	if err != nil {
+		panic(err)
+	}
+
+	for step := range steps {
+		msgBus[step.Name] = make(chan Task, 1000)
+
+		parallel := 0
+		if step.Parallel == nil {
+			parallel = maxParallel / int(stepsCount)
+		} else {
+			parallel = *step.Parallel
+		}
+
+		for range parallel {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				defer close(msgBus[step.Name])
+
+				tasksChan := db.GetUnprocessedTasks(step.ID)
+
+				for task := range chans.Merge(msgBus[step.Name], tasksChan) {
+					resourcePool <- struct{}{}        // acquire
+					defer func() { <-resourcePool }() // release
+
+					nextTasks := p.ExecuteTask(task)
+					numberOfExecutions++
+
+					for nextTask := range nextTasks {
+						msgBus[step.Name] <- nextTask
+					}
+				}
+			}()
+		}
+	}
+
+	var startStep *Step
+
+	if startStepName == "" {
+		startStep, err = db.GetStartingStep()
+	} else {
+		startStep, err = db.GetStepByName(startStepName)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	msgBus[startStep.Name] <- Task{
+		StepID: &startStep.ID,
+	}
+
+	wg.Wait()
+	return numberOfExecutions
+}
+
+func (p Pipeline) ExecuteTask(t Task) chan Task {
+	db := p.db
+
+	step, err := db.GetStep(*t.StepID)
+	if err != nil {
+		panic(err)
+	}
+
+	runLogger.Printf("Processing task %d for step '%s'", t.ID, step.Name)
+
+	t.Processed = true
+
+	inputFile, err := os.CreateTemp("/tmp", "input-*")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(inputFile.Name())
+
+	if t.ObjectHash != "" {
+		objectPath := db.GetObjectPath(t.ObjectHash)
+		data, err := os.Open(objectPath)
+		if err != nil {
+			panic(err)
+		}
+		n, err := io.Copy(inputFile, data)
+		if err != nil {
+			panic(err)
+		}
+		runLogger.Printf("  Input: %d bytes from %s", n, t.ObjectHash[:16]+"...")
+
+	} else {
+		runLogger.Println("  Input: (empty - start step)")
+	}
+	inputFile.Close()
+
+	outputDir, err := os.MkdirTemp("/tmp", "output-*")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(outputDir)
+
+	runLogger.Printf("  Executing script for step '%s'", step.Name)
+	cmd := exec.Command("sh", "-c", step.Script)
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("INPUT_FILE=%s", inputFile.Name()),
+		fmt.Sprintf("OUTPUT_DIR=%s", outputDir),
+	)
+
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	stderrPipe, err := cmd.StderrPipe()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		runLogger.Printf("  Error starting script: %v", err)
+		panic(err)
+	}
+
+	scriptLogger := log.New(os.Stderr, fmt.Sprintf("[SCRIPT:%s] ", step.Name), log.Ldate|log.Ltime|log.Lmsgprefix)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stdoutPipe)
+		for scanner.Scan() {
+			scriptLogger.Println(scanner.Text())
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stderrPipe)
+		for scanner.Scan() {
+			scriptLogger.Printf("[stderr] %s", scanner.Text())
+		}
+	}()
+
+	wg.Wait()
+
+	if err := cmd.Wait(); err != nil {
+		runLogger.Printf("  Error executing script: %v", err)
+	}
+
+	entries, err := os.ReadDir(outputDir)
+	if err != nil {
+		panic(err)
+	}
+
+	entriesChan := make(chan os.DirEntry)
+
+	go func() {
+		defer close(entriesChan)
+		for _, entry := range entries {
+			entriesChan <- entry
+		}
+	}()
+
+	newCount := 0
+	skippedCount := 0
+
+	outputTasks := make(chan Task)
+
+	go func() {
+		defer close(outputTasks)
+
+		workers.Parallel0(entriesChan, runtime.NumCPU(), func(entry os.DirEntry) {
+			if entry.IsDir() {
+				return
+			}
+
+			filename := entry.Name()
+			stepName := extractStepName(filename)
+			filePath := fmt.Sprintf("%s/%s", outputDir, filename)
+
+			nextStep, err := db.GetStepByName(stepName)
+			if err != nil {
+				panic(err)
+			}
+
+			isCompleted, err := db.IsTaskCompletedInNextStep(nextStep.ID, t.ID)
+			if err != nil {
+				panic(err)
+			}
+
+			if isCompleted {
+				fmt.Printf("This step is already completed %s\n", t.ID)
+				skippedCount++
+				return
+			}
+
+			runLogger.Printf("	Output: %s -> step '%s'", filename, stepName)
+
+			hash, err := hashFileSHA256(filePath)
+			if err != nil {
+				panic(err)
+			}
+
+			outputTasks <- Task{
+				ObjectHash:  hash,
+				StepID:      &nextStep.ID,
+				InputTaskID: &t.ID,
+				Processed:   isCompleted,
+			}
+
+			objectPath := db.GetObjectPath(hash)
+			_, err = copyFileWithSHA256(filePath, objectPath)
+			if err != nil {
+				panic(err)
+			}
+
+			_, err = db.CreateTask(hash, &nextStep.ID, &t.ID)
+			if err != nil {
+				panic(err)
+			}
+
+			newCount++
+		})
+	}()
+
+	runLogger.Printf("  Created %d new tasks, %d already existed", newCount, skippedCount)
+
+	return outputTasks
+}
--- run.go
+++ run.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"strings"
-	"sync"
 )
 
 var runLogger = log.New(os.Stderr, "[RUN] ", log.Ldate|log.Ltime|log.Lmsgprefix)
@@ -26,300 +22,184 @@ func extractStepName(filename string) string {
 }
 
 func run(manifest Manifest, database Database, parallel int, startStepName string) {
-	runLogger.Println("Registering steps...")
+
 	for _, step := range manifest.Steps {
-		if step.Start {
-			runLogger.Printf("Step: %s (START STEP)", step.Name)
-		} else {
-			runLogger.Printf("Step: %s", step.Name)
-		}
-		if step.Parallel != nil {
-			runLogger.Printf("  Parallel limit: %d", *step.Parallel)
-		}
-		database.RegisterStep(step.Name, step.Script, step.Start, step.Parallel)
+		database.CreateStep(step.Name, step.Script, step.Start, step.Parallel)
 	}
 
-	// Determine which step to start from
-	var startStep *Step
-	var err error
+	pipeline := NewPipeline(&database)
 
-	if startStepName != "" {
-		runLogger.Printf("Starting from step: %s", startStepName)
-		startStep, err = database.GetStepByName(startStepName)
-		if err != nil {
-			panic(err)
-		}
-		if startStep == nil {
-			panic(fmt.Sprintf("Step '%s' not found", startStepName))
-		}
-
-		// Mark all tasks for this step as unprocessed to re-run them
-		count, err := database.MarkStepTasksUnprocessed(startStepName)
-		if err != nil {
-			panic(err)
-		}
-		if count > 0 {
-			runLogger.Printf("Marked %d existing tasks as unprocessed for step '%s'", count, startStepName)
-		} else {
-			// No existing tasks, create an initial empty one
-			_, _, err := database.InsertTask("", &startStep.ID, nil)
-			if err != nil {
-				panic(err)
-			}
-			runLogger.Printf("Created initial task for step '%s' (no existing tasks found)", startStep.Name)
-		}
-	} else {
-		startStep, err = database.GetStartStep()
-		if err != nil {
-			panic(err)
-		}
-		if startStep == nil {
-			panic("No start step found in manifest")
-		}
-		runLogger.Printf("Starting from default start step: %s", startStep.Name)
-
-		// Create initial task for the start step if it doesn't exist
-		_, isNew, err := database.InsertTask("", &startStep.ID, nil)
-		if err != nil {
-			panic(err)
-		}
-		if isNew {
-			runLogger.Printf("Created initial task for step '%s'", startStep.Name)
-		} else {
-			runLogger.Printf("Initial task for step '%s' already exists", startStep.Name)
-		}
+	totalExecCount := 0
+	execCount := 1
+	for execCount > 0 {
+		c := pipeline.Execute(startStepName, parallel)
+		execCount = c
+		totalExecCount += c
 	}
 
-	// Track semaphores for per-step parallelism limits
-	stepSemaphores := make(map[int64]chan struct{})
-	var semMutex sync.Mutex
-
-	var wg sync.WaitGroup
-	jobs := make(chan Task, parallel)
-
-	// Worker function that respects per-step parallelism
-	processWithLimit := func(task Task, db Database) {
-		// Get step to check for parallelism limit
-		step, err := db.GetStepByID(*task.StepID)
-		if err != nil {
-			panic(err)
-		}
-
-		// Acquire slot from step-specific semaphore if limit is set
-		var sem chan struct{}
-		if step.Parallel != nil {
-			semMutex.Lock()
-			if stepSemaphores[step.ID] == nil {
-				stepSemaphores[step.ID] = make(chan struct{}, *step.Parallel)
-			}
-			sem = stepSemaphores[step.ID]
-			semMutex.Unlock()
-
-			sem <- struct{}{}        // Acquire
-			defer func() { <-sem }() // Release
-		}
-
-		task.deriveTasks(db)
-	}
-
-	for i := 0; i < parallel; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for task := range jobs {
-				processWithLimit(task, database)
-			}
-		}()
-	}
-
-	totalProcessed := 0
-	for {
-		unprocessed, err := database.GetUnprocessedTasks()
-		if err != nil {
-			panic(err)
-		}
-
-		if len(unprocessed) == 0 {
-			break
-		}
-
-		runLogger.Printf("Processing %d unprocessed tasks...", len(unprocessed))
-		for _, task := range unprocessed {
-			jobs <- task
-			totalProcessed++
-		}
-
-		close(jobs)
-		wg.Wait()
-
-		jobs = make(chan Task, parallel)
-		for i := 0; i < parallel; i++ {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				for task := range jobs {
-					processWithLimit(task, database)
-				}
-			}()
-		}
-	}
-
-	close(jobs)
-	wg.Wait()
-
-	runLogger.Printf("Completed processing %d tasks", totalProcessed)
+	runLogger.Printf("Completed processing %d tasks", totalExecCount)
 }
 
-func (t Task) deriveTasks(db Database) {
-	step, err := db.GetStepByID(*t.StepID)
-	if err != nil {
-		panic(err)
-	}
+// func (t Task) deriveTasks(db Database) {
+// 	step, err := db.GetStepByID(*t.StepID)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	runLogger.Printf("Processing task %d for step '%s'", t.ID, step.Name)
+// 	runLogger.Printf("Processing task %d for step '%s'", t.ID, step.Name)
 
-	err = db.MarkTaskProcessed(t.ID)
-	if err != nil {
-		panic(err)
-	}
+// 	err = db.MarkTaskProcessed(t.ID)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	inputFile, err := os.CreateTemp("/tmp", "input-*")
-	if err != nil {
-		panic(err)
-	}
-	defer os.Remove(inputFile.Name())
+// 	inputFile, err := os.CreateTemp("/tmp", "input-*")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer os.Remove(inputFile.Name())
 
-	if t.ObjectHash != "" {
-		objectPath := db.GetObjectPath(t.ObjectHash)
-		data, err := os.ReadFile(objectPath)
-		if err != nil {
-			panic(err)
-		}
-		runLogger.Printf("  Input: %d bytes from %s", len(data), t.ObjectHash[:16]+"...")
-		_, err = inputFile.Write(data)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		runLogger.Println("  Input: (empty - start step)")
-	}
-	inputFile.Close()
+// 	if t.ObjectHash != "" {
+// 		objectPath := db.GetObjectPath(t.ObjectHash)
+// 		data, err := os.ReadFile(objectPath)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		runLogger.Printf("  Input: %d bytes from %s", len(data), t.ObjectHash[:16]+"...")
+// 		_, err = inputFile.Write(data)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 	} else {
+// 		runLogger.Println("  Input: (empty - start step)")
+// 	}
+// 	inputFile.Close()
 
-	outputDir, err := os.MkdirTemp("/tmp", "output-*")
-	if err != nil {
-		panic(err)
-	}
-	defer os.RemoveAll(outputDir)
+// 	outputDir, err := os.MkdirTemp("/tmp", "output-*")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer os.RemoveAll(outputDir)
 
-	runLogger.Printf("  Executing script for step '%s'", step.Name)
-	cmd := exec.Command("sh", "-c", step.Script)
-	cmd.Env = append(os.Environ(),
-		fmt.Sprintf("INPUT_FILE=%s", inputFile.Name()),
-		fmt.Sprintf("OUTPUT_DIR=%s", outputDir),
-	)
+// 	runLogger.Printf("  Executing script for step '%s'", step.Name)
+// 	cmd := exec.Command("sh", "-c", step.Script)
+// 	cmd.Env = append(os.Environ(),
+// 		fmt.Sprintf("INPUT_FILE=%s", inputFile.Name()),
+// 		fmt.Sprintf("OUTPUT_DIR=%s", outputDir),
+// 	)
 
-	// Capture stdout and stderr
-	stdoutPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	stderrPipe, err := cmd.StderrPipe()
-	if err != nil {
-		panic(err)
-	}
+// 	// Capture stdout and stderr
+// 	stdoutPipe, err := cmd.StdoutPipe()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	stderrPipe, err := cmd.StderrPipe()
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	// Start the command
-	if err := cmd.Start(); err != nil {
-		runLogger.Printf("  Error starting script: %v", err)
-		panic(err)
-	}
+// 	// Start the command
+// 	if err := cmd.Start(); err != nil {
+// 		runLogger.Printf("  Error starting script: %v", err)
+// 		panic(err)
+// 	}
 
-	// Create a script logger for this specific step
-	scriptLogger := log.New(os.Stderr, fmt.Sprintf("[SCRIPT:%s] ", step.Name), log.Ldate|log.Ltime|log.Lmsgprefix)
+// 	// Create a script logger for this specific step
+// 	scriptLogger := log.New(os.Stderr, fmt.Sprintf("[SCRIPT:%s] ", step.Name), log.Ldate|log.Ltime|log.Lmsgprefix)
 
-	// Stream stdout
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		scanner := bufio.NewScanner(stdoutPipe)
-		for scanner.Scan() {
-			scriptLogger.Println(scanner.Text())
-		}
-	}()
+// 	// Stream stdout
+// 	var wg sync.WaitGroup
+// 	wg.Add(2)
+// 	go func() {
+// 		defer wg.Done()
+// 		scanner := bufio.NewScanner(stdoutPipe)
+// 		for scanner.Scan() {
+// 			scriptLogger.Println(scanner.Text())
+// 		}
+// 	}()
 
-	// Stream stderr
-	go func() {
-		defer wg.Done()
-		scanner := bufio.NewScanner(stderrPipe)
-		for scanner.Scan() {
-			scriptLogger.Printf("[stderr] %s", scanner.Text())
-		}
-	}()
+// 	// Stream stderr
+// 	go func() {
+// 		defer wg.Done()
+// 		scanner := bufio.NewScanner(stderrPipe)
+// 		for scanner.Scan() {
+// 			scriptLogger.Printf("[stderr] %s", scanner.Text())
+// 		}
+// 	}()
 
-	// Wait for output streaming to complete
-	wg.Wait()
+// 	// Wait for output streaming to complete
+// 	wg.Wait()
 
-	// Wait for command to complete
-	if err := cmd.Wait(); err != nil {
-		runLogger.Printf("  Error executing script: %v", err)
-		// panic(err)
-	}
+// 	// Wait for command to complete
+// 	if err := cmd.Wait(); err != nil {
+// 		runLogger.Printf("  Error executing script: %v", err)
+// 		// panic(err)
+// 	}
 
-	entries, err := os.ReadDir(outputDir)
-	if err != nil {
-		panic(err)
-	}
+// 	entries, err := os.ReadDir(outputDir)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	newCount := 0
-	skippedCount := 0
-	for _, entry := range entries {
-		if entry.IsDir() {
-			continue
-		}
+// 	entriesChan := make(chan os.DirEntry)
 
-		filename := entry.Name()
-		stepName := extractStepName(filename)
-		filePath := fmt.Sprintf("%s/%s", outputDir, filename)
+// 	go func() {
+// 		defer close(entriesChan)
+// 		for _, entry := range entries {
+// 			entriesChan <- entry
+// 		}
+// 	}()
 
-		runLogger.Printf("  Output: %s -> step '%s'", filename, stepName)
+// 	newCount := 0
+// 	skippedCount := 0
 
-		targetStep, err := db.GetStepByName(stepName)
-		if err != nil {
-			runLogger.Printf("    Warning: Error looking up step '%s': %v", stepName, err)
-			continue
-		}
+// 	workers.Parallel0(entriesChan, runtime.NumCPU(), func(entry os.DirEntry) {
+// 		if entry.IsDir() {
+// 			return
+// 		}
 
-		var stepID *int64
-		if targetStep != nil {
-			stepID = &targetStep.ID
-		} else {
-			runLogger.Printf("    (terminal output - no step '%s')", stepName)
-			// stepID remains nil for terminal results
-		}
+// 		filename := entry.Name()
+// 		stepName := extractStepName(filename)
+// 		filePath := fmt.Sprintf("%s/%s", outputDir, filename)
 
-		hash, err := hashFileSHA256(filePath)
-		if err != nil {
-			panic(err)
-		}
+// 		runLogger.Printf("  Output: %s -> step '%s'", filename, stepName)
 
-		objectPath := db.GetObjectPath(hash)
-		_, err = copyFileWithSHA256(filePath, objectPath)
-		if err != nil {
-			panic(err)
-		}
+// 		targetStep, err := db.GetStepByName(stepName)
+// 		if err != nil {
+// 			runLogger.Printf("    Warning: Error looking up step '%s': %v", stepName, err)
+// 			return
+// 		}
 
-		_, isNew, err := db.InsertTask(hash, stepID, &t.ID)
-		if err != nil {
-			panic(err)
-		}
+// 		var stepID *int64
+// 		if targetStep != nil {
+// 			stepID = &targetStep.ID
+// 		} else {
+// 			runLogger.Printf("    (terminal output - no step '%s')", stepName)
+// 			// stepID remains nil for terminal results
+// 		}
 
-		if isNew {
-			newCount++
-		} else {
-			skippedCount++
-		}
-	}
+// 		hash, err := hashFileSHA256(filePath)
+// 		if err != nil {
+// 			panic(err)
+// 		}
 
-	runLogger.Printf("  Created %d new tasks, %d already existed", newCount, skippedCount)
-}
+// 		objectPath := db.GetObjectPath(hash)
+// 		_, err = copyFileWithSHA256(filePath, objectPath)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		_, isNew, err := db.InsertTask(hash, stepID, &t.ID)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		if isNew {
+// 			newCount++
+// 		} else {
+// 			skippedCount++
+// 		}
+// 	})
+
+// 	runLogger.Printf("  Created %d new tasks, %d already existed", newCount, skippedCount)
+// }
