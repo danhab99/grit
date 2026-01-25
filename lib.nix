fb2096efe187e8fabde3989c5f32236a5dbcae10
Dan Habot <dan.habot@gmail.com>
2026-01-25 01:06:55 -0500

    format

--- lib.nix
+++ lib.nix
@@ -17,14 +17,14 @@
       isAttrSet = a: builtins.hasAttr a args;
     in
     ''
-[[step]]
-name="${name}"
-script='''
-${script}
+      [[step]]
+      name="${name}"
+      script='''
+      ${script}
 '''
-${if isStart then "start=true" else ""}
-${if ( isAttrSet "parallel" ) then "parallel=${builtins.toString parallel}" else ""}
-${if ( isAttrSet "inputs" ) then "inputs=[${builtins.concatStringsSep ", " (builtins.map (x: "\"${x}\"") inputs)}]" else ""}
+      ${if isStart then "start=true" else ""}
+      ${if ( isAttrSet "parallel" ) then "parallel=${builtins.toString parallel}" else ""}
+      ${if ( isAttrSet "inputs" ) then "inputs=[${builtins.concatStringsSep ", " (builtins.map (x: "\"${x}\"") inputs)}]" else ""}
     '';
 
   version = "1.0";
