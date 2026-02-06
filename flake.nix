b2dbaa86ccdafb27a468a912c3eaaff50d17430b
Dan Habot <dan.habot@gmail.com>
2026-02-05 23:47:29 -0500

    dynamically calculated version based on changelog

--- flake.nix
+++ flake.nix
@@ -4,8 +4,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    (flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -19,9 +25,9 @@
         packages = {
           default = pkgs.buildGoModule {
             pname = "grit";
-            version = "0.2.2";
+            version = import ./changelog;
             src = self;
-            vendorHash = "sha256-NEWUHUio0oPZdSB9obpZEOD5RQcIsAwnosQg2yESXME=";
+            vendorHash = "sha256-t8nxIZPT7Ki4lvGhXBp3o7cl4klhKkxc26KgjUX9djw=";
             subPackages = [ "." ];
 
             GO_PATH = "${self.outPath}/.go";
@@ -50,14 +56,19 @@
           CGO_CPPFLAGS = "-U_FORTIFY_SOURCE";
         };
 
-        checks = import ./checks.nix { inherit pkgs grit lib; };
-      })) // (let
+        checks = import ./nix/checks.nix { inherit pkgs grit lib; };
+      }
+    ))
+    // (
+      let
         pkgs = import nixpkgs {
           system = "x86_64-linux";
           config.allowUnfree = true;
         };
         lib = pkgs.lib;
-      in {
-        lib = import ./lib.nix { inherit lib pkgs; };
-      });
+      in
+      {
+        lib = import ./nix/lib.nix { inherit lib pkgs; };
+      }
+    );
 }
